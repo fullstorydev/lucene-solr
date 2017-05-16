5edc69e2af57057805729e38db63396ea3eeb429
Scott Blum <dragonsinth@gmail.com>
2017-05-16 19:44:28 -0400

    fs.ignoreconflicts on update

--- solr/core/src/java/org/apache/solr/update/processor/DistributedUpdateProcessor.java
+++ solr/core/src/java/org/apache/solr/update/processor/DistributedUpdateProcessor.java
@@ -784,6 +784,7 @@ public class DistributedUpdateProcessor extends UpdateRequestProcessor {
     for (String p : whitelist) {
       passParam(params, fparams, p);
     }
+    passParam(params, fparams, "fs.ignoreconflicts");
     return fparams;
   }
 
