1ae6a07a60f9cca0aadc9b1ab943f5699615478b
Josh Humphries <jh@fullstory.com>
2017-09-28 14:07:53 -0400

    add stats to see if the pool is being used; simple little test case in main method

--- solr/core/src/java/org/apache/solr/search/facet/FixedBitSetPool.java
+++ solr/core/src/java/org/apache/solr/search/facet/FixedBitSetPool.java
@@ -18,6 +18,7 @@ package org.apache.solr.search.facet;
  */
 
 import java.util.Arrays;
+import java.util.Map;
 import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
@@ -72,9 +73,17 @@ class FixedBitSetPool {
   }
 
   private final AtomicLong totalSize = new AtomicLong();
+  private final AtomicLong poolHits = new AtomicLong();
+  private final AtomicLong poolHitsSize = new AtomicLong();
+  private final AtomicLong poolMisses = new AtomicLong();
+  private final AtomicLong poolMissesSize = new AtomicLong();
+  private final AtomicLong poolReturns = new AtomicLong();
+  private final AtomicLong poolReturnsSize = new AtomicLong();
+  private final AtomicLong poolReturnsRejected = new AtomicLong();
+  private final AtomicLong poolReturnsRejectedSize = new AtomicLong();
   private final ConcurrentSkipListMap<Integer, ConcurrentStack<long[]>> entries = new ConcurrentSkipListMap<>();
 
-  private static final class ConcurrentStack<T> extends AtomicReference<ConcurrentStack<T>> {
+  private static final class ConcurrentStack<T> {
     private static final class Node<T> extends AtomicReference<Node<T>> {
       private final T val;
       Node(T val) {
@@ -128,14 +137,19 @@ class FixedBitSetPool {
       long[] arr = stack.pop();
       if (arr != null) {
         Arrays.fill(arr, 0);
-        totalSize.addAndGet(-arr.length);
+        totalSize.addAndGet(-(arr.length<<3));
         if (stack.isEmpty()) {
           entries.remove(arr.length, stack);
         }
+        poolHits.incrementAndGet();
+        poolHitsSize.addAndGet(arr.length);
         return new FixedBitSet(arr, numBits);
       }
     }
-    return new FixedBitSet(numBits);
+    FixedBitSet ret = new FixedBitSet(numBits);
+    poolMisses.incrementAndGet();
+    poolMissesSize.addAndGet(ret.getBits().length);
+    return ret;
   }
 
   public void put(FixedBitSet set) {
@@ -143,9 +157,11 @@ class FixedBitSetPool {
 
     while (true) {
       long sz = totalSize.get();
-      long newSz = sz + (arr.length>>3);
+      long newSz = sz + (arr.length<<3);
       if (newSz > MAX_POOL_SIZE) {
         // pool is full
+        poolReturnsRejected.incrementAndGet();
+        poolReturnsRejectedSize.addAndGet(arr.length);
         return;
       }
       if (totalSize.compareAndSet(sz, newSz)) {
@@ -160,12 +176,62 @@ class FixedBitSetPool {
         ConcurrentStack<long[]> newStack = new ConcurrentStack<>(arr);
         stack = entries.putIfAbsent(len, newStack);
         if (stack == null) {
+          poolReturns.incrementAndGet();
+          poolReturnsSize.addAndGet(arr.length);
           return;
         }
       }
       if (stack.push(arr)) {
-        break;
+        poolReturns.incrementAndGet();
+        poolReturnsSize.addAndGet(arr.length);
+        return;
       }
     }
+  }
+
+  public static void main(String[] args) {
+    System.out.println("MAX = " + MAX_POOL_SIZE + " (" + Runtime.getRuntime().maxMemory() + ")");
+    FixedBitSetPool p = getPool();
+    printStats(p);
+    FixedBitSet fbs = p.get(100);
+    printStats(p);
+    p.put(fbs);
+    printStats(p);
+    fbs = p.get(100);
+    printStats(p);
+    p.put(fbs);
+    printStats(p);
+    fbs = p.get(1000000000);
+    printStats(p);
+    p.put(fbs);
+    printStats(p);
+    fbs = p.get(2000000000);
+    printStats(p);
+    p.put(fbs);
+    printStats(p);
+  }
+
+  private static void printStats(FixedBitSetPool p) {
+    System.out.println("CURRENT SIZE = " + p.totalSize.get());
+    System.out.println("hits = " + p.poolHits.get() + " (" + p.poolHitsSize.get() + ")");
+    System.out.println("misses = " + p.poolMisses.get() + " (" + p.poolMissesSize.get() + ")");
+    System.out.println("returns = " + p.poolReturns.get() + " (" + p.poolReturnsSize.get() + ")");
+    System.out.println("rejects = " + p.poolReturnsRejected.get() + " (" + p.poolReturnsRejectedSize.get() + ")");
+    for (Map.Entry<Integer, ConcurrentStack<long[]>> entry : p.entries.entrySet()) {
+      int c = countLen(entry.getValue());
+      if (c > 0) {
+        System.out.println("  " + entry.getKey() + ": " + c);
+      }
+    }
+  }
+
+  private static int countLen(ConcurrentStack<?> s) {
+    int count = 0;
+    ConcurrentStack.Node<?> n = s.head.get();
+    while (n != null) {
+      count++;
+      n = n.get();
+    }
+    return count;
   }
 }
