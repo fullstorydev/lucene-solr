7f8b95d15e8bf40549891de1cbadf76f5822fa39
Noble Paul <noblepaul@users.noreply.github.com>
2019-06-24 22:20:42 +1000

    SOLR-13534: Dynamic loading of jars from a url (#30)
    SOLR-13534 : Dynamic loading to support loading jars from a URL

--- solr/core/src/java/org/apache/solr/core/SolrCore.java
+++ solr/core/src/java/org/apache/solr/core/SolrCore.java
@@ -995,6 +995,9 @@ public final class SolrCore implements SolrInfoBean, SolrMetricProducer, Closeab
       // Initialize the RestManager
       restManager = initRestManager();
 
+      // at this point we can load jars loaded from remote urls.
+      memClassLoader.loadRemoteJars();
+
       // Finally tell anyone who wants to know
       resourceLoader.inform(resourceLoader);
       resourceLoader.inform(this); // last call before the latch is released.
