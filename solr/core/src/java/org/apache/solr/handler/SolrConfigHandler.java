a36222ce42de4bb64c45c6ec9bc5adb4e25f4947
Emily Bragg <ebragg09@gmail.com>
2021-02-07 22:01:19 -0500

    debug

--- solr/core/src/java/org/apache/solr/handler/SolrConfigHandler.java
+++ solr/core/src/java/org/apache/solr/handler/SolrConfigHandler.java
@@ -266,7 +266,11 @@ public class SolrConfigHandler extends RequestHandlerBase implements SolrCoreAwa
                     log.info("info found is {}", info);
                     if (info.type.equals(parts.get(1)) && info.name.equals(componentName)) {
                       log.info("component name {} info.name {} o {} type {}", componentName, info.name, o, o.getClass().getName());
-                      o.attributes.put("_packageinfo_", listener.getPackageVersion());
+                      if (o instanceof PluginInfo) {
+                        o.attributes.put("_packageinfo_", listener.getPackageVersion());
+                      } else {
+                        log.info("{} was not instance of PluginInfo", o);
+                      }
                     }
                   }
                 }
